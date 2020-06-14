4d76adbc732523b5e7898b627db4b063c5e25fc7
fhaards <m.fahmi37@gmail.com>
2020-06-14 16:43:29 +0700

    Modified user

---
+++ application/views/_adminpages/konfirmasi/form_konfirmasi_pembayaran.php
@@ -0,0 +1,22 @@
+<div class="app-page-title">
+    <div class="page-title-wrapper">
+        <div class="page-title-heading">
+            <div>
+                <div class="page-title mt-0">
+                    <h2>Konfirmasi Pembayaran</h2>
+                </div>
+                <div class="page-title-subheading">
+                    <strong class="ml-2"><?= $logo['nm_apl']; ?></strong>
+                </div>
+            </div>
+        </div>
+        <div class="page-title-actions">
+        </div>
+    </div>
+</div>
+
+<div class="row mb-5">
+    <div class="col-md-12">
+
+    </div>
+</div>
